78bdde8768b39e7ac32b99c40e556be03ff3a3b5
BG-27 <ghosh2207100@stud.kuet.ac.bd>
2025-12-05 18:01:40 +0600

    scntdn

--- Non-linear Equations/Secant method/Code/Secant Method.cpp
+++ Non-linear Equations/Secant method/Code/Secant Method.cpp
@@ -7,12 +7,12 @@ double f(double x,const vector<double>& coeffs){
     for(int i=0;i<=n;i++){rslt +=coeffs[i]*pow(x,n-i);}
     return rslt;}
 
-double deriv(double x,const vector<double>& coeffs){
+/* double deriv(double x,const vector<double>& coeffs){
     double rslt=0;
     int n=coeffs.size()-1;
     for(int i=0;i<n;i++){rslt+=coeffs[i]*(n-i)*pow(x,n-i-1);}
     return rslt;
-}
+} */
 
 void printeq(const vector<double>& coeffs,ofstream& outFile) {
     int n=coeffs.size()-1;
@@ -41,8 +41,8 @@ for(int i=0;i<n;i++){if(coeffs[i] !=0){
             }else{outFile<<abs(coeffs[i]*(n-i))<<"x^"<<n-i-1;}}}
     outFile<<"\n";}
 
-double secant(double x0, double x1, double e, int maxiter,const vector<double>& coeffs, ofstream& outFile)
-{ double f0 = f(x0, coeffs),f1 = f(x1, coeffs);
+double secant(double x0, double x1, double e,int maxiter,const vector<double>& coeffs, ofstream& outFile)
+{ double f0 = f(x0,coeffs),f1 = f(x1,coeffs);
 
     for(int iter=1;iter<=maxiter;iter++)
     {if(fabs(f1-f0)< 1e-12)
@@ -53,6 +53,7 @@ double secant(double x0, double x1, double e, int maxiter,const vector<double>& 
 
         if(fabs(x2-x1)<e)
             return x2;
+
         x0 = x1;
         f0 = f1;
         x1 = x2;
@@ -66,22 +67,23 @@ int main(){
     ifstream inFile("input.txt");ofstream outFile("output.txt");
 
     if (!inFile){
-        cerr <<"Error opening input file!" << "\n";
+        cerr<<"Error opening input file!"<<"\n";
         return -1;}
 
         int degree;inFile>>degree;
 
-    vector<double> coeffs(degree + 1);
+    vector<double> coeffs(degree+1);
     for(int i=0;i<=degree;i++){inFile>>coeffs[i];}
 
-    outFile << "The equation is: ";
+    outFile<<"The equation is: ";
     printeq(coeffs, outFile);
-    outFile << "The derivative is: ";
+    outFile<<"The derivative is: ";
     printderiv(coeffs, outFile);
-    double guess,e;
+    double x0,e;
     int maxiter;
-    inFile >> guess >> e >> maxiter;
-    double root = newtonraphson(guess,e,maxiter,coeffs,outFile);
+    inFile>>x0>>e>>maxiter;
+    double x1 =x0+0.5;// bg fr nw u doing it hardcoded in future u can take here x1 frm user it means x0 x1 both user input
+    double root = secant(x0,x1,e,maxiter,coeffs,outFile);
     outFile<<"\nThe root is: "<<setprecision(6)<<root<<"\n";
     inFile.close();
     outFile.close();
